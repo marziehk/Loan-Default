7ef2cbf41df78c519aa1e3ecf49a4949d66a7a77
M-Ghodrat <ghodrat@ualberta.ca>
2021-04-29 16:27:27 -0600

    update EDA missing values

--- EDA/Missing Values/missing.py
+++ EDA/Missing Values/missing.py
@@ -1,32 +1,38 @@
+import pandas as pd
+import numpy as np
+from sklearn import linear_model
+
 class missingvalue():
 
-    def __init__(self,missing_columns,data):
-        self.missing_columns=missing_columns
-        self.data=data
+    def __init__(self, *, data, missCol):
+        self.missCol = missCol
+        self.data = data.copy()
 
     @staticmethod
     def random_imputation(data, features):
         number_missing = data[features].isnull().sum()
         observed_values = data.loc[data[features].notnull(), features]
-        data.loc[data[features].isnull(), features + '_imp'] = np.random.choice(observed_values, number_missing, replace = True)
+        data.loc[data[features].isnull(), features + '_imp'] = np.random.choice(observed_values, size=number_missing, replace = True)
         return data
 
     def random_imp(self):
-        for feature in self.missing_columns:
+        for feature in self.missCol:
             self.data[feature + '_imp'] = self.data[feature]
             self.data = self.random_imputation(self.data, feature)
         return self.data
 
-    def regressionpart(self):
-        deter_data = pd.DataFrame(columns = ["Det" + name for name in self.missing_columns])
+    def regression_random(self, parameters=None):
+        deter_data = pd.DataFrame(columns = ["Det_" + name for name in self.missCol])
         self.data = self.random_imp()
-        for feature in self.missing_columns:
-            deter_data["Det" + feature] = self.data[feature + "_imp"]
-            parameters = list(set(self.data.columns) - set(self.missing_columns) - {feature + '_imp'})
-            # Create a Linear Regression model to estimate the missing data
+        for feature in self.missCol:
+            if not parameters:
+                parameters = list(set(self.data.columns) - set(self.missCol) - {feature + '_imp'})
+            deter_data["Det_" + feature] = self.data[feature + "_imp"]
+            #Create a Linear Regression model to estimate the missing data
             model = linear_model.LinearRegression()
-            model.fit(X = self.data[parameters], y = self.data[feature + '_imp'])
+            model.fit(X=self.data[parameters], y=self.data[feature + '_imp'])
             # Observe that I preserve the index of the missing data from the original dataframe
-            deter_data.loc[self.data[feature].isnull(), "Det" + feature] = model.predict(self.data[parameters])[self.data[feature].isnull()]
-        return pd.concat([self.data,deter_data],axis=1)
+            deter_data.loc[self.data[feature].isnull(), "Det_" + feature] = model.predict(self.data[parameters])[self.data[feature].isnull()]
+        return pd.concat([self.data, deter_data], axis=1)
+
         #return deter_data
